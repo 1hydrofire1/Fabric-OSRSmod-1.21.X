5bb69836c586453108d9f5831659e45abbea3cff
1hydrofire1 <1hydrofire1@gmail.com>
2025-06-17 10:26:54 -0500

    added useless comment

--- src/main/java/net/nsverdrup/osrsmod/OSRSMod.java
+++ src/main/java/net/nsverdrup/osrsmod/OSRSMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// not a very important comment, can be deleted
 public class OSRSMod implements ModInitializer {
 	public static final String MOD_ID = "osrsmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
