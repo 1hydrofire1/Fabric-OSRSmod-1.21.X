a1850ed6fafb908fe1c81ca4327565dcf6e5ca36
1hydrofire1 <1hydrofire1@gmail.com>
2025-06-17 15:15:45 -0500

    added raw and non raw pink garnet

--- src/main/java/net/nsverdrup/osrsmod/OSRSMod.java
+++ src/main/java/net/nsverdrup/osrsmod/OSRSMod.java
@@ -2,6 +2,7 @@ package net.nsverdrup.osrsmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.nsverdrup.osrsmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class OSRSMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
