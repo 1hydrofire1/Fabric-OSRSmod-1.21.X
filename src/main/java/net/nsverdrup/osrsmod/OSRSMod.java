804e948e99d4d9489c46c8eb56f8056ead88f21b
1hydrofire1 <1hydrofire1@gmail.com>
2025-06-17 10:24:45 -0500

    Initial commit

---
+++ src/main/java/net/nsverdrup/osrsmod/OSRSMod.java
@@ -0,0 +1,16 @@
+package net.nsverdrup.osrsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OSRSMod implements ModInitializer {
+	public static final String MOD_ID = "osrsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nsverdrup/osrsmod/OSRSModClient.java
@@ -0,0 +1,10 @@
+package net.nsverdrup.osrsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OSRSModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
