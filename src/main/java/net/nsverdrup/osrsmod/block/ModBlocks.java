e71a9bc5266b24528f775023ffbb00454b8942a2
1hydrofire1 <1hydrofire1@gmail.com>
2025-06-19 15:37:08 -0500

    Added custom tag for magic block

--- src/main/java/net/nsverdrup/osrsmod/block/ModBlocks.java
+++ src/main/java/net/nsverdrup/osrsmod/block/ModBlocks.java
@@ -1,11 +1,11 @@
 package net.nsverdrup.osrsmod.block;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.ExperienceDroppingBlock;
-import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.nsverdrup.osrsmod.OSRSMod;
+import net.nsverdrup.osrsmod.block.custom.MagicBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,7 +13,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.nsverdrup.osrsmod.block.custom.MagicBlock;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -32,6 +32,7 @@ public class ModBlocks {
 
     public static final Block MAGIC_BLOCK = registerBlock("magic_block",
             new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
